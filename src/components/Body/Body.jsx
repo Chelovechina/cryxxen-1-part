c7d1800ae2b81372aa537a509b22fae6b0282e66
Chelovechina <zhkk2004@gmail.com>
2022-12-04 15:53:46 +0600

    refactor: rename body styles into first section scss

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,4 +1,5 @@
-import styles from './Body.module.scss';
+import firstStyle from './FirstSection.module.scss';
+
 import img from './../../assets/images/1.png';
 import play from './../../assets/play-btn.svg';
 import appStore from './../../assets/appstore-btn.svg';
@@ -6,35 +7,38 @@ import googlePlay from './../../assets/googleplay-btn.svg';
 
 const Body = () => {
   return (
-    <section className={styles.section}>
-      <div className="container">
-        <div className={styles.section__wrapper}>
-          <div className={styles.info}>
-            <div className={styles.info__play}>
-              <img src={play} alt="play img" />
-              <p className={styles.info__subtext}>In every beginning, there is chaos.</p>
+    <>
+      <section className={firstStyle.firstSection}>
+        <div className="container">
+          <div className={firstStyle.firstSection__wrapper}>
+            <div className={firstStyle.info}>
+              <div className={firstStyle.info__play}>
+                <img src={play} alt="play img" />
+                <p className={firstStyle.info__subtext}>In every beginning, there is chaos.</p>
+              </div>
+              <h2 className={firstStyle.info__title}>Peaceful shopping.</h2>
+              <h2 className={firstStyle.info__title}>Mindful money.</h2>
+              <p className={firstStyle.info__text}>
+                See how we can help with making your shopping experience and money management more
+                ZEN.
+              </p>
+              <div className={firstStyle.info__btns}>
+                <button className={firstStyle.info__btn}>
+                  <img src={appStore} alt="App Store" />
+                </button>
+                <button className={firstStyle.info__btn}>
+                  <img src={googlePlay} alt="Google Play" />
+                </button>
+              </div>
             </div>
-            <h2 className={styles.info__title}>Peaceful shopping.</h2>
-            <h2 className={styles.info__title}>Mindful money.</h2>
-            <p className={styles.info__text}>
-              See how we can help with making your shopping experience and money management more
-              ZEN.
-            </p>
-            <div className={styles.info__btns}>
-              <button className={styles.info__btn}>
-                <img src={appStore} alt="App Store" />
-              </button>
-              <button className={styles.info__btn}>
-                <img src={googlePlay} alt="Google Play" />
-              </button>
+            <div className={firstStyle.section__img}>
+              <img src={img} alt="Zen app" />
             </div>
-          </div>
-          <div className={styles.section__img}>
-            <img src={img} alt="Zen app" />
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="secondSection"></section>
+    </>
   );
 };
 
